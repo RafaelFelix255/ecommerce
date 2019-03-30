17de169928f58a58657e8bdaa6180ac4c23e350a
Rafael Felix <rafaelribeirofelix@gmail.com>
2019-03-30 18:05:49 -0300

    Criando o PageAdmin e o TPL Admin

--- index.php
+++ index.php
@@ -4,22 +4,20 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	//$sql = new Hcode\DB\sql();
-	//$results = $sql->select("select * from tb_users u");
-	//echo json_encode($results);
-
 	$page = new Page();
 	$page->setTpl("index");
+});
 
-
-
+$app->get('/admin', function() {
+	$page = new PageAdmin();
+	$page->setTpl("index");
 });
 
 $app->run();
