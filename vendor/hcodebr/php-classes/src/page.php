614422d582a156278824036569a74428b666144d
Rafael Felix <rafaelribeirofelix@gmail.com>
2019-03-30 21:23:20 -0300

    Criado a tela de login

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -9,6 +9,8 @@ class page{
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
@@ -30,7 +32,9 @@ class page{
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if ($this->options["header"] === true) {
+            $this->tpl->draw("header");
+        }
 
     }
 
@@ -55,7 +59,9 @@ class page{
 
     public function __destruct(){
         
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] === true) {
+            $this->tpl->draw("footer");
+        }
 
     }
 
