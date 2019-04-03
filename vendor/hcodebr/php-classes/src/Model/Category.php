89c96fd49c673a41fb9fd02f3b23bf38c528f27a
Rafael Felix <rafaelribeirofelix@gmail.com>
2019-04-03 00:32:52 -0300

    Criado Categoria VS Produtos

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -59,6 +59,52 @@ class Category extends Model {
         
         file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR."categories-menu.html", implode('', $html));
     }
+
+    public function getProducts($related = true){
+        $sql = new Sql();
+
+        if ($related === true){
+            return $sql->select("select * from tb_products a where a.idproduct in (
+                                    select a.idproduct
+                                      from tb_products a, tb_productscategories b
+                                     where a.idproduct = b.idproduct
+                                       and b.idcategory = :idcategory);",
+                            
+                                [':idcategory'=>$this->getidcategory()
+            ]);
+        } else {
+            return $sql->select("select * from tb_products a where a.idproduct not in (
+                                    select a.idproduct
+                                      from tb_products a, tb_productscategories b
+                                     where a.idproduct = b.idproduct
+                                       and b.idcategory = :idcategory);",
+                            
+                                [':idcategory'=>$this->getidcategory()
+            ]);
+        }
+    }
+
+    public function addProduct(Product $product){
+        $sql = new Sql();
+        $sql->query("insert into tb_productscategories
+                        (idcategory, idproduct)
+                    values
+                        (:idcategory, :idproduct);", [
+                            ':idcategory'=>$this->getidcategory(),
+                            ':idproduct'=>$product->getidproduct()
+                    ]);
+    }
+
+    public function removeProduct(Product $product){
+        $sql = new Sql();
+        $sql->query("delete from tb_productscategories
+                      where idcategory = :idcategory
+                        and idproduct = :idproduct;", [
+                            ':idcategory'=>$this->getidcategory(),
+                            ':idproduct'=>$product->getidproduct()
+                    ]);
+    }
+
 }
 
 ?>
